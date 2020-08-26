5c24017ad05ee38ea6645879bc9dc4670acb3e61
Sarah Beckley <sarahswebdev@gmail.com>
2020-08-26 14:30:40 -0700

    initial commit

---
+++ process.php
@@ -0,0 +1,18 @@
+<?php
+  if (isset($_POST['email']))  {
+  
+    //Email information
+    $admin_email = "slbhasnmp10@gmail.com";
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $phone = $_POST['phone'];
+    $message = $_POST['message'];
+    
+    //send email
+    mail($admin_email, "New Form Submission", $message . ' - ' . $phone, "From:" . $email);
+    
+    header('Location: http://edgeledger.net/success.html');
+  }
+
+  
+
